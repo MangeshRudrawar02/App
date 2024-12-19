4ee09bb5865fc1db89d2d85e4d49b5a91c32885c
MangeshRudrawar02 <rudrawar.mangesh02@gmail.com>
2024-12-20 01:56:02 +0530

    second commit

--- my-app/src/dbConfig/dbConfig.ts
+++ my-app/src/dbConfig/dbConfig.ts
@@ -1,11 +1,19 @@
 import mongoose from 'mongoose';
 
-export async function Connect() {
-
-    try{
+export async function connect() {
+    try {
         mongoose.connect(process.env.MONGO_URI!)
-    }catch(error){
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log("Connected to MongoDB");
+        })
+        connection.on('error', (err) => {
+            console.log("falied Connect to MongoDB");
+            process.exit();
+        })
+    } catch (error) {
         console.log(error);
     }
-    
+
 }
