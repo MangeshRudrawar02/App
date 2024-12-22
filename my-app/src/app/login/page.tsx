0270fec8dfd4ce48fb524d660e22f7bf4603331b
MangeshRudrawar02 <rudrawar.mangesh02@gmail.com>
2024-12-22 19:09:51 +0530

    Update page.tsx
    commit after branch is published

--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -2,8 +2,9 @@ export default function LoginPage(){
     return(
         <div className="flex">
             <h1 className="text-center ">
-                Hello My name is manegsh and this change has been done through abcd branch
+                Hello My name is manegsh and this change has been done through abcd branch.
             </h1>
+            <h1 className="text-center ">this is change after publishing/ pushing first time my feature banch</h1>
         </div>
     )
 }
