fdecf36f561f11e5d46d4522bf86a8943019ad57
MangeshRudrawar02 <rudrawar.mangesh02@gmail.com>
2024-12-30 00:14:22 +0530

    com3

--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -2,9 +2,9 @@ export default function LoginPage(){
     return(
         <div className="flex">
             <h1 className="text-center ">
-                Hello My name is manegsh and this change has been done through feature second part branch.
+                Hello My name is mangesh and this change has been done through feature second part branch.
             </h1>
-            <h1 className="text-center ">this is change after publishing/ pushing first time my feature 2nd part banch</h1>
+            
         </div>
     )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -10,34 +10,36 @@ export default function SignUpPage() {
         email: "",
         password: "",
         username: "",
-    }}
+    })
 
     const onSignup = async () => {
     }
     return (
-        <div className="felx felx-col items-center jsutify-center min-h-screen py-2"> 
-            <h1>Sign Up</h1>
-            <input
+        <div className="flex flex-col items-center justify-center h-screen"> 
+            <h1 className="p-3">Sign Up</h1>
+            <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 type="text"
                 placeholder="Email"
                 value={user.email}
                 onChange={(e) => setUser({ ...user, email: e.target.value })}
             />
-            <input
+            <input  className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 type="password"
                 placeholder="Password"
                 value={user.password}
                 onChange={(e) => setUser({ ...user, password: e.target.value })}
             />
-            <input
+            <input  className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 type="text"
                 placeholder="Username"
                 value={user.username}
                 onChange={(e) => setUser({ ...user, username: e.target.value })}
             />
-            <button onClick={onSignup}>Sign Up</button>
-            <Link href="/login">Login</Link>
+            <button className="p-2 bg-blue-500 text-white rounded-lg"
+            onClick={onSignup}>Sign Up</button>
+            <Link className="p-3" href="/login">Login</Link>
         </div>
+
         
 
     )
